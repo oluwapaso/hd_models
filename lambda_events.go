24ceb3c1bee7d1167e4b18771bb9baaf745537c7
oluwapaso <adebayoishola01@gmail.com>
2023-08-28 09:48:35 +0100

    Updated json Tags

--- lambda_events.go
+++ lambda_events.go
@@ -1,13 +1,13 @@
 package models
 
 type MailerEvent struct {
-	Server        string `json:"name"`
+	Server        string `json:"server"`
 	From_Email    string `json:"from_email"`
 	From_Name     string `json:"from_name"`
 	To_Email      string `json:"to_email"`
 	To_Name       string `json:"to_name"`
-	Subject       string `json:"suject"`
-	Body          string `json:"email_body"`
+	Subject       string `json:"subject"`
+	Email_Body    string `json:"email_body"`
 	SMTP_Host     string `json:"smtp_host"`
 	SMTP_Port     int    `json:"smtp_port"`
 	SMTP_Username string `json:"smtp_username"`
