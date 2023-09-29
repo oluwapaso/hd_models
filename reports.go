9d6b20ae4cbf1908929010018f7ad1dc33f71a21
oluwapaso <adebayoishola01@gmail.com>
2023-09-29 17:10:30 +0100

    Updates

--- reports.go
+++ reports.go
@@ -6,3 +6,18 @@ type AddExpensesParams struct {
 	Amount       string
 	Descriptions string
 }
+
+type ListExpensesParams struct {
+	Company_Id int
+	StartFrom  int
+	Limit      int
+}
+
+type ExpensesList struct {
+	Expenses_Id int    `json:"expenses_id"`
+	Company_Id  int    `json:"company_id"`
+	Duration    string `json:"duration"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Amount      string `json:"amount"`
+}
