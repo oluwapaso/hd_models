3a8af24904317e027d24c19324e64342f16f6843
oluwapaso <adebayoishola01@gmail.com>
2024-02-12 11:54:00 +0100

    Updates

---
+++ central_dispatch.go
@@ -0,0 +1,26 @@
+package models
+
+import "database/sql"
+
+type Check_CD_HD_Carrier_Params struct {
+	CD_Carrier_Id  string
+	CD_Carrier_UId string
+}
+
+type Add_CD_Saved_Carrier_Params struct {
+	Username     string
+	Carrier_Id   string
+	Carrier_UId  string
+	Company_Name string
+	Auth_State   string
+	MC_Num       string
+	Contact      string
+	Address      string
+	City         string
+	State        string
+	Zip          string
+	Phone_1      string
+	Phone_2      string
+	Fax          string
+	Tx           *sql.Tx
+}
